dc2be3ae7840584216ef213afbb71648137201c9
fedir72 <fedir72@ua.fm>
2022-08-24 12:29:58 +0300

    the start project from book

---
+++ HIITFit/HIITFit/ContentView.swift
@@ -0,0 +1,17 @@
+/// Copyright (c) 2021 Razeware LLC
+///
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
