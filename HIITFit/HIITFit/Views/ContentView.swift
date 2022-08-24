270b4f7291f0383aa8300653bb24bc652d59a8d1
fedir72 <fedir72@ua.fm>
2022-08-24 13:37:57 +0300

    less.2 create and setup tab view

---
+++ HIITFit/HIITFit/Views/ContentView.swift
@@ -0,0 +1,30 @@
+/// Copyright (c) 2021 Razeware LLC
+///
+
+import SwiftUI
+
+struct ContentView: View {
+
+    
+    
+    var body: some View {
+        TabView{
+        WelcomeView()
+            ForEach(0 ..< 4) { index in
+             ExerciseView(index: index)
+           }
+        }
+       // .tabViewStyle(.page(indexDisplayMode: .never))
+        .tabViewStyle(.page(indexDisplayMode: .always))
+        .indexViewStyle(.page(backgroundDisplayMode: .always))
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+      ContentView()
+         
+  }
+}
+
